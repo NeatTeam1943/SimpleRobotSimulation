5270819b144439806d9f87dcff2d208867045cf5
Guy Avital <guyavital10@gmail.com>
2022-04-07 18:22:52 +0300

    Add robot template files (Documented)

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.auto.AutoDrive2Sec;
+import frc.robot.commands.auto.AutoTurn90Deg;
+import frc.robot.commands.auto.AutoTurn90DegPID;
+import frc.robot.subsystems.Drivetrain;
+
+/**
+ * This class is where the bulk of the robot should be declared.
+ */
+public class RobotContainer {
+
+    private final Joystick m_stick = new Joystick(0);
+    private final SendableChooser<Command> m_autoChooser = new SendableChooser<>();
+
+    /**
+     * The container for the robot. Contains subsystems, IO devices, and commands.
+     */
+    public RobotContainer() {
+        configureAutoChooser();
+        configureButtonBindings();
+        configureDefaultCommands();
+    }
+
+    /**
+     * Use this method to configure a chooser for autonomous commands.
+     * The chooser will be displayed on the dashboard.
+     */
+    private void configureAutoChooser() {
+        SmartDashboard.putData("Auto", m_autoChooser);
+    }
+
+    /**
+     * Use this method to define your button -> command mappings.
+     */
+    private void configureButtonBindings() {
+    }
+
+    /**
+     * Use this method to configure default commands for subsystems.
+     * For example an arcade drive command for the drivetrain that uses
+     * a joystick to control the robot.
+     */
+    private void configureDefaultCommands() {
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     * 
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return m_autoChooser.getSelected();
+    }
+}
